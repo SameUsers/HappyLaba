883a7ce62560ed49d59fb1ab2a9c4fe79c4153df
Alex Rusin <sasandro85@mail.ru>
2026-07-06 11:10:59 +0300

    Change main.py

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from bootstrap import AppBuilder
 
 async def main():
     server = AppBuilder.build_app()
-    await server.start()
+    try:
+        await server.start()
+    except (KeyboardInterrupt, asyncio.CancelledError):
+        await server.stop()
 
 if __name__ == "__main__":
     asyncio.run(main())
